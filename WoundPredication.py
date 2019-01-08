e459793cdeabc970966ff82a3af6602e779e51e4
msthil <46465101+msthil@users.noreply.github.com>
2019-01-07 20:49:09 -0600

    Update WoundPredication.py

--- WoundPredication.py
+++ WoundPredication.py
@@ -1,3 +1,4 @@
+import random as r
 class Battle:
     
     def __init__(self, tohit, towound, tosave, rend, attacks):
@@ -28,12 +29,14 @@ tohit = int(input('Enter the roll needed to hit: '))
 towound = int(input('Enter the roll needed to wound: '))  
 tosave = int(input('Enter the roll needed to save: '))
 rend = int(input('Enter rending value of attack: '))
+maxdamage = int(input('Enter max damage possible: '))
 
 unit = Battle(tohit, towound, tosave, rend, attacks)
 
 hit = 0
 wound = 0
 woundthrough = 0
+damage = 0
 for i in range(0,100000):
     if(r.randrange(1,7) >= unit.gettohit()):
         hit = hit + 1
@@ -41,6 +44,10 @@ for i in range(0,100000):
             wound = wound + 1
             if(r.randrange(1,7) < (unit.gettosave() + unit.getrend())):
                 woundthrough = woundthrough + 1
+                if maxdamage == 1:
+                    damage = damage + 1
+                else:
+                    damage = damage + r.randrange(1,maxdamage + 1)
             
 print("\nChance to hit: {:.2f}".format((hit / 100000) * 100 ), '%')
 print("Chance to wound: {:.2f}".format((wound / 100000) * 100), '%')
@@ -48,4 +55,8 @@ print("Chance wound goes through: {:.2f}".format((woundthrough/100000) * 100), '
 
 print('Likely # of hits: ', (unit.getattacks() * hit) / 100000)
 print('Likely # of  wound: ', (unit.getattacks() * wound) / 100000)
-print('Likely # of wound that go through: ', (unit.getattacks() * woundthrough)/100000)
+print('Likely # of wounds that go through: ', (unit.getattacks() * woundthrough)/100000)
+if maxdamage > 1:
+    print('Damage expected: ', (damage)/100000)
+else:
+    print('Damage expected: ', (unit.getattacks() * woundthrough)/100000)
